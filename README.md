d181d9cbbbc6d62c959533924d56e692491f7515
Sorin <93950240+Sorin373@users.noreply.github.com>
2023-03-21 11:36:26 +0200

    Update

--- README.md
+++ README.md
@@ -1,1 +1,110 @@
+# Problem Question
+- Consider a rectangular game board, divided into n lines and m columns. In this way, n*m zones are obtained and the height of each zone is known. At a known position - line istart, column jstart there is a ball that can move to a neighboring position (up, down, left, right) only if the height of the neighboring position is strictly lower than the height of the current position.
+- Determine the maximum number of zones the ball can pass through to reach one of the edges of the game board.
 
+
+# Input
+- The input file _board.txt_ contains the numbers n and m on the first line. The next n lines contain m strictly positive natural numbers representing the heights of each zone.
+On line n+2 there are two numbers istart, jstart with the meaning in the statement.
+~~~
+4 5
+4 4 3 1 5
+8 7 2 1 3
+9 6 3 2 1
+2 5 4 3 7
+2 2
+~~~
+
+# Output
+- On the screen it will be shows the maximum number of zones through which the ball can pass to reach one of the edges of the game board, including the starting zone.
+~~~
+5
+~~~
+
+# Restrictions and clarifications
+- 1 ≤ n, m ≤ 20
+- rows and columns are numbered from 1
+- the height of each zone is between 1 and 50
+- when the ball reaches an edge of the board, it stops there. It does not continue on that edge and cannot return to the inside of the board.
+- if it is not possible for the ball to reach the edge of the board, a message will be displayed
+
+# Solution
+- I created a reccursive backtracking algorithm which explores all possible paths in each of the four directions (up, down, left, right), if the condition is met.
+~~~
+{
+        if (Board[i + 1][j] < Board[i][j])
+        {
+            Path[i + 1][j] = true;
+            back(i + 1, j, k + 1);
+            Path[i + 1][j] = false;
+        }
+
+        if (Board[i - 1][j] < Board[i][j])
+        {
+            Path[i - 1][j] = true;
+            back(i - 1, j, k + 1);
+            Path[i - 1][j] = false;
+        }
+
+        if (Board[i][j + 1] < Board[i][j])
+        {
+            Path[i][j + 1] = true;
+            back(i, j + 1, k + 1);
+            Path[i][j + 1] = false;
+        }
+
+        if (Board[i][j - 1] < Board[i][j])
+        {
+            Path[i][j - 1] = true;
+            back(i, j - 1, k + 1);
+            Path[i][j - 1] = false;
+        }
+}
+~~~
+- After a solution is found (the ball reached the edge of the board) I calculate _maxim_ which at the end of the program will contain the maximum number of zones through which the ball passed.
+~~~
+if (solutie(i, j))
+    {
+        if (k == 1)
+            Path[i][j] = true;
+
+        tiparSolutie(k);
+
+        if (k > maxim)
+        {
+            maxim = k;
+            for (unsigned int i = 1; i <= n; i++)
+                for (unsigned int j = 1; j <= m; j++)
+                    PathMax[i][j] = Path[i][j];
+        }
+
+        Path[i][j] = false;
+    }
+~~~
+- In addition to the problem's reqirements I also displayed all the paths created, in the file _traseu.txt_.
+~~~
+Numarul zonelor trecute: 3
+Traseu:
+
+0 0 0 0 0 
+0 1 0 0 0 
+0 1 0 0 0 
+0 1 0 0 0 
+
+Numarul zonelor trecute: 5
+Traseu:
+
+0 0 0 0 0 
+0 1 0 0 0 
+0 1 1 1 1 
+0 0 0 0 0 
+
+Numarul zonelor trecute: 2
+Traseu:
+
+0 1 0 0 0 
+0 1 0 0 0 
+0 0 0 0 0 
+0 0 0 0 0 
+
+~~~
--- main.cpp
+++ main.cpp
@@ -3,16 +3,16 @@
 
 using namespace std;
 
-int tabla[25][25], n, m, istart, jstart, maxim;
-bool traseu[25][25], traseuMax[25][25];
+int Board[25][25], n, m, istart, jstart, maxim;
+bool Path[25][25], PathMax[25][25];
 
 void citireDate()
 {
-    ifstream fin("tabla.txt");
+    ifstream fin("board.txt");
     fin >> n >> m;
     for (unsigned int i = 1; i <= n; i++)
         for (unsigned int j = 1; j <= m; j++)
-            fin >> tabla[i][j];
+            fin >> Board[i][j];
     fin >> istart >> jstart;
     fin.close();
 }
@@ -23,7 +23,7 @@ void afisareDateInitiale()
     for (unsigned int i = 1; i <= n; i++)
     {
         for (unsigned int j = 1; j <= m; j++)
-            cout << tabla[i][j] << " ";
+            cout << Board[i][j] << " ";
         cout << '\n';
     }
     cout << '\n';
@@ -32,21 +32,21 @@ void afisareDateInitiale()
 void resetFisierTraseu()
 {
     ofstream fout;
-    fout.open("traseu.txt", ofstream::out | ofstream::trunc);
+    fout.open("path.txt", ofstream::out | ofstream::trunc);
     fout.close();
 }
 
 void tiparSolutie(int k)
 {
     ofstream fout;
-    fout.open("traseu.txt", ios::app);
+    fout.open("path.txt", ios::app);
     fout << "Numarul zonelor trecute: " << k << '\n'
          << "Traseu:"
          << "\n\n";
     for (unsigned int i = 1; i <= n; i++)
     {
         for (unsigned int j = 1; j <= m; j++)
-            fout << traseu[i][j] << " ";
+            fout << Path[i][j] << " ";
         fout << '\n';
     }
     fout << '\n';
@@ -65,7 +65,7 @@ void back(int i, int j, int k)
     if (solutie(i, j)) // daca am ajuns la margine -> soltutie -> calculam nr. maxim de mutari
     {
         if (k == 1)
-            traseu[i][j] = true;
+            Path[i][j] = true;
 
         tiparSolutie(k);
 
@@ -74,42 +74,42 @@ void back(int i, int j, int k)
             maxim = k;
             for (unsigned int i = 1; i <= n; i++)
                 for (unsigned int j = 1; j <= m; j++)
-                    traseuMax[i][j] = traseu[i][j];
+                    PathMax[i][j] = Path[i][j];
         }
 
-        traseu[i][j] = false;
+        Path[i][j] = false;
     }
     else
     {
         // se apeleaza recursiv functia back cu noile coordonate si nr de pasi actualizat
         // daca pozitia este valida
-        traseu[i][j] = true;
-        if (tabla[i + 1][j] < tabla[i][j])
+        Path[i][j] = true;
+        if (Board[i + 1][j] < Board[i][j])
         {
-            traseu[i + 1][j] = true;    // marcam pozitia pe unde am trecut
+            Path[i + 1][j] = true;    // marcam pozitia pe unde am trecut
             back(i + 1, j, k + 1);
-            traseu[i + 1][j] = false;   // resetam pozitia
+            Path[i + 1][j] = false;   // resetam pozitia
         }
 
-        if (tabla[i - 1][j] < tabla[i][j])
+        if (Board[i - 1][j] < Board[i][j])
         {
-            traseu[i - 1][j] = true;
+            Path[i - 1][j] = true;
             back(i - 1, j, k + 1);
-            traseu[i - 1][j] = false;
+            Path[i - 1][j] = false;
         }
 
-        if (tabla[i][j + 1] < tabla[i][j])
+        if (Board[i][j + 1] < Board[i][j])
         {
-            traseu[i][j + 1] = true;
+            Path[i][j + 1] = true;
             back(i, j + 1, k + 1);
-            traseu[i][j + 1] = false;
+            Path[i][j + 1] = false;
         }
 
-        if (tabla[i][j - 1] < tabla[i][j])
+        if (Board[i][j - 1] < Board[i][j])
         {
-            traseu[i][j - 1] = true;
+            Path[i][j - 1] = true;
             back(i, j - 1, k + 1);
-            traseu[i][j - 1] = false;
+            Path[i][j - 1] = false;
         }
     }
 }
@@ -131,7 +131,7 @@ int main()
         for (unsigned int i = 1; i <= n; i++)
         {
             for (unsigned int j = 1; j <= m; j++)
-                cout << traseuMax[i][j] << " ";
+                cout << PathMax[i][j] << " ";
             cout << '\n';
         }
     }
